3264177baa3f7b0f58bf81d2846456cff9097536
Kuro watoshi <pyanz.jheo123@gmail.com>
2024-11-20 01:34:54 +0800

    Fixed Landing Page Design -- Admin Page Design (NOT YET)

--- Migrations/20241119133117_New.Designer.cs
+++ Migrations/20241119133117_New.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BrgyLink.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241119083258_UpdateResidentModel")]
-    partial class UpdateResidentModel
+    [Migration("20241119133117_New")]
+    partial class New
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
