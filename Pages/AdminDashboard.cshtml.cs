b0814d0956e978c1a53359ce6f3ccd64ed38f509
Kuro watoshi <pyanz.jheo123@gmail.com>
2024-12-12 01:18:57 +0800

    Admin Layout done, Manage Resident Design Done, Officials none

--- Pages/AdminDashboard.cshtml.cs
+++ Pages/AdminDashboard.cshtml.cs
@@ -18,10 +18,15 @@ namespace BrgyLink.Pages
 
         // Property to hold the total number of residents
         public int TotalResidents { get; set; }
+        public int TotalOfficials { get; set; }
+        // Property for calculating progress percentages
+        public double ResidentsProgress => TotalResidents; // No max value, just the total
+        public double OfficialsProgress => TotalOfficials;
 
         // OnGetAsync method to fetch data
         public async Task OnGetAsync()
         {
+            TotalOfficials = await _context.BarangayOfficials.CountAsync();
             // Retrieve the total count of residents from the database
             TotalResidents = await _context.Residents.CountAsync(); // Fetch the count of residents
         }
